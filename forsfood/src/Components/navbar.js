b0cb173c21503c37c3dcfd90acb8ad0bbdc4bd11
ericGOD24 <ericobandeira@hotmail.com>
2018-11-22 02:03:07 -0200

    adicionando navbar estranha

---
+++ forsfood/src/Components/navbar.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import '../Style/navbar.css';
+
+class Navbar extends Component {
+    render() {
+        return(
+            <div className="navbar">
+                <div className ="text">
+                  <h1>For'sFood</h1>   
+                </div>
+            </div>
+        )
+    }
+}
+
+export default Navbar
