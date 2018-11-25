7a46854747b61bf695463493d8de2005e22d4a96
ericGOD24 <ericobandeira@hotmail.com>
2018-11-25 17:57:13 -0200

    adicionando cards e fixando navbar

--- forsfood/src/Components/login_feed.js
+++ forsfood/src/Components/login_feed.js
@@ -1,46 +1,77 @@
 import React, { Component } from 'react';
 import '../Style/login_feed.css';
 
+
 class Login_feed extends Component {
   render() {
     return (
       <div class="col s12 m7">
-        <div class="card horizontal">
+        <div class="card horizontal caca">
           <div class="card-image">
             <img className="foto" src={require('../img/burgao.jpeg')} alt="Foto Burgao"/>
           </div>
           <div class="card-stacked">
-            <div class="card-content">
-              <p>Irmão, eu sou a porra da descrição da competição.</p>
+            <div class="card-content spc">
+              <h4 class = "txt">Desafio Medievalanches</h4>
+              <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet"/>
+              <p>
+                <i class="tiny material-icons">home</i>
+                 restaurante: Medieval Lanches
+              </p>
+              <p>
+                <i class="tiny material-icons">location_on</i>
+                 Localização: Ceilandia Sul
+              </p>
+              <p>
+                <i class="tiny material-icons">access_alarm</i>
+                 tempo: 30 minutos (limite)
+              </p>
+              <p>
+                <i class="tiny material-icons">room_service</i>
+                 Peso: 1.5 Kg
+              </p>
+              <p>
+                <i class="tiny material-icons">restaurant_menu</i>
+                Número de vitórias: 2
+              </p>
             </div>
             <div class="card-action">
-              <a href="#">This is AMERICA</a>
+              <a href="#">Clique aqui para mais detalhes</a>
             </div>
           </div>
         </div>
-        <div class="card horizontal">
+
+        <div class="card horizontal caca">
           <div class="card-image">
-            <img src="https://lorempixel.com/100/190/nature/6" />
+            <img className="foto" src={require('../img/vermonster.jpg')} alt="Foto Vermonster"/>
           </div>
           <div class="card-stacked">
-            <div class="card-content">
-              <p>Irmão, eu sou a porra da descrição de alguma coisa.</p>
+            <div class="card-content spc">
+              <h4 class = "txt">Desafio Vermonster</h4>
+              <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet"/>
+              <p>
+                <i class="tiny material-icons">home</i>
+                restaurante: Ben & Jerry's
+              </p>
+              <p>
+                <i class="tiny material-icons">location_on</i>
+                Localização: Park Shopping
+              </p>
+              <p>
+                <i class="tiny material-icons">access_alarm</i>
+                tempo: 5 minutos (limite)
+              </p>
+              <p>
+                <i class="tiny material-icons">room_service</i>
+                Peso: 900 Kg
+              </p>
+              <p>
+                <i class="tiny material-icons">restaurant_menu</i>
+                Número de vitórias: 3
+              </p>
             </div>
             <div class="card-action">
-              <a href="#">This is patrick</a>
-            </div>
-          </div>
-        </div>
-        <div class="card horizontal">
-          <div class="card-image">
-            <img src="https://lorempixel.com/100/190/nature/6" />
-          </div>
-          <div class="card-stacked">
-            <div class="card-content">
-              <p>Irmão, eu sou a porra da descrição de alguma coisa.</p>
-            </div>
-            <div class="card-action">
-              <a href="#">This is a link</a>
+              <a href="#">Clique aqui para mais detalhes</a>
             </div>
           </div>
         </div>
--- forsfood/src/Components/navbar.js
+++ forsfood/src/Components/navbar.js
@@ -4,30 +4,32 @@ import '../Style/navbar.css';
 class Navbar extends Component {
     render() {
         return(
-            <nav>
-                <div class="nav-wrapper nav">
-                <a href="#!" class="coco">For'sFood</a>
-                    <ul class="right hide-on-med-and-down nav">
-                        <li>
-                            <div class="row">
-                                <div class="input-field col s10">
-                                <input id="first_name2" type="text" class="validate"/>
-                                <label class="active" for="first_name2">Login</label>
+            <div class="navbar-fixed">
+                <nav>
+                    <div class="nav-wrapper nav">
+                    <a href="#!" class="coco">For'sFood</a>
+                        <ul class="right hide-on-med-and-down nav">
+                            <li>
+                                <div class="row">
+                                    <div class="input-field col s10">
+                                    <input id="first_name2" type="text" class="validate"/>
+                                    <label class="active" for="first_name2">Login</label>
+                                    </div>
                                 </div>
-                            </div>
-                        </li>
-                        <li>
-                            <div class="row">
-                                <div class="input-field col s10">
-                                <input id="password" type="password" class="validate"/>
-                                <label class="active" for="first_name2">Senha</label>
+                            </li>
+                            <li>
+                                <div class="row">
+                                    <div class="input-field col s10">
+                                    <input id="password" type="password" class="validate"/>
+                                    <label class="active" for="first_name2">Senha</label>
+                                    </div>
                                 </div>
-                            </div>
-                        </li>
-                        <li><a class="waves-effect waves-light btn grey">Entrar</a></li>
-                    </ul>
-                </div>
-            </nav>
+                            </li>
+                            <li><a class="waves-effect waves-light btn grey">Entrar</a></li>
+                        </ul>
+                    </div>
+                </nav>
+            </div>
         )
     }
 }
